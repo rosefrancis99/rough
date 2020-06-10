e1ef4cbd2456a581571cff885f38188626fc5402
rosefrancis99 <65714045+rosefrancis99@users.noreply.github.com>
2020-06-10 14:11:47 +0000

    Add files via upload

---
+++ fronts.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+ <head>
+   <title>Welcome</title>
+   <style>
+   	h1{color: firebrick;
+   		font-size: 800%;
+   		font-family: verdana;
+   	   text-align: center;
+   	   margin-top: 250px;} 
+   	body{background-image: url('b4.jpg');
+   			background-repeat: no-repeat;
+   			background-attachment: fixed;
+   			background-size: cover;}
+   	.button{border: none;color: black;text-align: center;font-size: 20px;font-family: calibri;}
+   	.button1{background-color: grey;margin-left: 700px; margin-right: 20px;}
+   	.button2{background-color: grey;}
+      a:link { color: black;text-decoration: none; }
+      a:visited {color: black; text-decoration: none; }
+      a:hover {color: white; text-decoration: none; }
+      a:active{color: black; text-decoration: none; }
+   	</style>
+  </head>
+   <body>
+   	<h1>THE LOUNGE</h1>
+   	<form>
+   		<button class="button button1"><a href="login.php">Login</a>
+   		<!--<input type="button" value="Login">-->
+   	    </button>
+   	    <button class="button button2"><a href="signup.php">
+   		<!--<input type="button" value="Sign Up">--> Signup</a>
+   	    </button>
+   </body>
+   </html>
